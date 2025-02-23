c28c1c4f9d156bf95d774938021d4283a4acb939
Andrew Auclair <mightymalakai33@gmail.com>
2025-02-22 22:31:24 -0500

    Use VCPKG_ROOT

--- backend/generate_visual_studio_sln.bat
+++ backend/generate_visual_studio_sln.bat
@@ -1,3 +1,3 @@
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-cmake -G "Visual Studio 17 2022" -S . -B cmake-build-vs2022 -DCMAKE_TOOLCHAIN_FILE="C:\Users\might\source\GitHub\vcpkg\scripts\buildsystems\vcpkg.cmake"
+cmake -G "Visual Studio 17 2022" -S . -B cmake-build-vs2022 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake"
