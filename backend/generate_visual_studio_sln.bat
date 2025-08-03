78337d5aea37f39758acd770efc81808ee74977c
Andrew Auclair <mightymalakai33@gmail.com>
2025-08-03 10:44:01 -0400

    Update generate_visual_studio_sln.bat

--- backend/generate_visual_studio_sln.bat
+++ backend/generate_visual_studio_sln.bat
@@ -4,4 +4,4 @@ IF EXIST "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\B
 	call "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common\7\Tools\VsDevCmd.bat"
 )
 
-cmake -G "Visual Studio 17 2022" -S . -B cmake-build-vs2022 -DCMAKE_TOOLCHAIN_FILE="%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake"
+cmake -G "Visual Studio 17 2022" -S . -B cmake-build-vs2022 -DCMAKE_TOOLCHAIN_FILE="..\vcpkg\scripts\buildsystems\vcpkg.cmake"
