16bc068218058008114f1fe6b3737ba6d507c835
Andrew Auclair <mightymalakai33@gmail.com>
2025-12-24 12:22:39 -0500

    Renumber Packets

--- backend/lib/packets/message.hpp
+++ backend/lib/packets/message.hpp
@@ -5,7 +5,6 @@
 #include <ostream>
 #include <vector>
 
-// TODO redo numbers when we're done
 enum class PacketType : std::int32_t
 {
 	VERSION_REQUEST = 1,
@@ -16,61 +15,65 @@ enum class PacketType : std::int32_t
 	START_TASK = 5,
 	STOP_TASK = 6,
 	FINISH_TASK = 7,
-	UPDATE_TASK = 15,
-	REQUEST_TASK = 22,
+	UPDATE_TASK = 8,
+	REQUEST_TASK = 9,
 
-	SUCCESS_RESPONSE = 8,
-	FAILURE_RESPONSE = 9,
+	EDIT_TASK_SESSION = 10,
+	ADD_TASK_SESSION = 11,
+	REMOVE_TASK_SESSION = 12,
+	
+	SUCCESS_RESPONSE = 13,
+	FAILURE_RESPONSE = 14,
 
-	REQUEST_CONFIGURATION = 10,
-	REQUEST_CONFIGURATION_COMPLETE = 11,
+	REQUEST_CONFIGURATION = 15,
+	REQUEST_CONFIGURATION_COMPLETE = 16,
 
-	TASK_INFO = 12,
+	TASK_INFO = 17,
 
 	// configure and refresh bugzilla (refresh is manual by the user on the UI)
 	// the server will create and update tasks based on bugzilla changes and send TASK_INFO messages
 	// both of these messages have responses of SUCCESS_RESPONSE or FAILURE_RESPONSE
-	BUGZILLA_INFO = 13,
-	BUGZILLA_REFRESH = 14,
+	BUGZILLA_INFO = 18,
+	BUGZILLA_REFRESH = 19,
 
 	// report for the day with various information, displayed on a dialog on the UI
-	DAILY_REPORT = 16,
-	REQUEST_DAILY_REPORT = 17,
+	DAILY_REPORT = 20,
+	REQUEST_DAILY_REPORT = 21,
 
 	// report for the time tracking for the week, displayed on a dialog on the UI
-	WEEKLY_REPORT = 18,
-	REQUEST_WEEKLY_REPORT = 19,
+	WEEKLY_REPORT = 22,
+	REQUEST_WEEKLY_REPORT = 23,
 
 	// request the server to search for tasks matching the provided information
-	SEARCH_REQUEST = 20,
+	SEARCH_REQUEST = 24,
 	// return the results to the UI. This is a list of task IDs matching the search request
-	SEARCH_RESULTS = 21,
+	SEARCH_RESULTS = 25,
 
 	// configure the backup, including the IP and port of the backup service, how often to perform a backup, and how many backups to keep
-	BACKUP_CONFIGURATION = 23,
+	BACKUP_CONFIGURATION = 26,
 	// backup has been successfully performed
-	BACKUP_PERFORMED = 24,
+	BACKUP_PERFORMED = 27,
 	// backup has failed. error message and last successful backup time are provided
-	BACKUP_FAILED = 25,
+	BACKUP_FAILED = 28,
 
-	TIME_ENTRY_REQUEST = 26,
-	TIME_ENTRY_DATA = 27,
-	TIME_ENTRY_MODIFY = 28,
+	TIME_ENTRY_REQUEST = 29,
+	TIME_ENTRY_DATA = 30,
+	TIME_ENTRY_MODIFY = 31,
 
-	START_UNSPECIFIED_TASK = 29,
-	STOP_UNSPECIFIED_TASK = 30,
-	UNSPECIFIED_TASK_ACTIVE = 38,
+	START_UNSPECIFIED_TASK = 32,
+	STOP_UNSPECIFIED_TASK = 33,
+	UNSPECIFIED_TASK_ACTIVE = 34,
 
-	BULK_TASK_UPDATE_START = 31,
-	BULK_TASK_UPDATE_FINISH = 32,
+	BULK_TASK_UPDATE_START = 35,
+	BULK_TASK_UPDATE_FINISH = 36,
 
-	BULK_TASK_INFO_START = 33,
-	BULK_TASK_INFO_FINISH = 34,
+	BULK_TASK_INFO_START = 37,
+	BULK_TASK_INFO_FINISH = 38,
 
-	BULK_TASK_ADD_START = 35,
-	BULK_TASK_ADD_FINISH = 36,
+	BULK_TASK_ADD_START = 39,
+	BULK_TASK_ADD_FINISH = 40,
 
-	ERROR_MESSAGE = 37,
+	ERROR_MESSAGE = 41,
 };
 
 struct Message
@@ -79,8 +82,6 @@ public:
 	Message(PacketType packetType) : m_packetType(packetType) {}
 
 	PacketType packetType() const { return m_packetType; }
-
-	virtual bool operator==(const Message& message) const = delete;// = 0;
 
 	virtual std::vector<std::byte> pack() const = 0;
 
@@ -94,11 +95,6 @@ public:
 private:
 	PacketType m_packetType;
 };
-
-//inline bool Message::operator==(const Message& message) const
-//{
-//	return m_packetType == message.m_packetType;
-//}
 
 inline std::ostream& Message::print(std::ostream& out) const
 {
