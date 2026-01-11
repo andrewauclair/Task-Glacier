9c7e2e1441be26a9c31182947fa5709c69dad67b
Andrew Auclair <mightymalakai33@gmail.com>
2026-01-11 16:17:55 -0500

    0.12.0

--- backend/lib/api.cpp
+++ backend/lib/api.cpp
@@ -19,7 +19,7 @@ void API::process_packet(const Message& message)
 	switch (message.packetType())
 	{
 	case PacketType::VERSION_REQUEST:
-		m_sender->send(std::make_unique<VersionMessage>("0.11.0"));
+		m_sender->send(std::make_unique<VersionMessage>("0.12.0"));
 		break;
 	case PacketType::CREATE_TASK:
 		create_task(static_cast<const CreateTaskMessage&>(message));
--- ui/src/main/java/dialogs/About.java
+++ ui/src/main/java/dialogs/About.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 public class About extends JDialog {
     public static String serverVersion;
-    private static final String uiVersion = "0.11.0";
+    private static final String uiVersion = "0.12.0";
 
     public About(MainFrame mainFrame) {
         super(mainFrame);
