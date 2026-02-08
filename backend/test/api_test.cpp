cdaf0d74abc36bffd786399caad9c6bc8799b821
Andrew Auclair <mightymalakai33@gmail.com>
2026-02-08 18:13:05 -0500

    Test

--- backend/test/api_test.cpp
+++ backend/test/api_test.cpp
@@ -1585,7 +1585,7 @@ TEST_CASE("Request Version", "[api]")
 
 	REQUIRE(sender.output.size() == 1);
 
-	verify_message(VersionMessage("0.14.0"), *sender.output[0]);
+	verify_message(VersionMessage(TASK_GLACIER_VERSION), *sender.output[0]);
 }
 
 TEST_CASE("Start Unspecified Task", "[api][task]")
