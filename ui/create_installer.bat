ea4dc572e09ae3159b38b3335ec8bb82f1e32513
Andrew Auclair <mightymalakai33@gmail.com>
2026-03-08 15:18:02 -0400

    Try Again
    We'll get the versioning right eventually.
    
    Also, remove storage_test.cpp, it's unused.

--- ui/create_installer.bat
+++ ui/create_installer.bat
@@ -1,7 +1,7 @@
-if not defined RELEASE_VERSION (
-    for /f "tokens=*" %%i in ('git describe --tags --always') do set RELEASE_VERSION=%%i
-)
+REM if no release version, set to 0.0.0 to satisfy jpackage
+REM jpackage requires semantic versioning and won't work with our git describe version (which will be shown in the UI About dialog)
+if not defined RELEASE_VERSION set RELEASE_VERSION=0.0.0
 
 call gradlew build
 
-jpackage --name "Task Glacier" --app-version "%RELEASE_VERSION%" --icon app-icon-64.ico -i build/libs --main-class taskglacier.MainFrame --main-jar task-glacier-ui.jar --win-menu --win-dir-chooser
+jpackage --name "Task Glacier" --app-version %RELEASE_VERSION% --icon app-icon-64.ico -i build/libs --main-class taskglacier.MainFrame --main-jar task-glacier-ui.jar --win-menu --win-dir-chooser
