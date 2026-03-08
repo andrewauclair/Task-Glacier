2598321f9e23d68f2427b9b5d776ff88f53b1571
Andrew Auclair <mightymalakai33@gmail.com>
2026-03-08 15:05:19 -0400

    Test

--- ui/create_installer.bat
+++ ui/create_installer.bat
@@ -4,4 +4,4 @@ if not defined RELEASE_VERSION (
 
 call gradlew build
 
-jpackage --name "Task Glacier" --app-version %RELEASE_VERSION% --icon app-icon-64.ico -i build/libs --main-class taskglacier.MainFrame --main-jar task-glacier-ui.jar --win-menu --win-dir-chooser
+jpackage --name "Task Glacier" --app-version "%RELEASE_VERSION%" --icon app-icon-64.ico -i build/libs --main-class taskglacier.MainFrame --main-jar task-glacier-ui.jar --win-menu --win-dir-chooser
